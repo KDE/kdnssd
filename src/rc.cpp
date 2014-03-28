d69c1aa223f03e5eda4edaaa6365cc2a464bf8f5
l10n daemon script <scripty@kde.org>
2014-03-28 01:56:06 +0000

    SVN_SILENT made messages (after extraction)

--- src/rc.cpp
+++
@@ -1,6 +0,0 @@
-// i18n: file: kcm_kdnssd.kcfg:9
-// i18n: ectx: label, entry, group (browsing)
-i18n("Additional domains for browsing");
-// i18n: file: kcm_kdnssd.kcfg:10
-// i18n: ectx: whatsthis, entry, group (browsing)
-i18n("List of 'wide-area' (non link-local) domains that should be browsed.");
